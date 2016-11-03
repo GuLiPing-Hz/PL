ec31355c968502bea20b128b46665c216ed14328
guliping <peace@zygames.net>
2016-11-03 17:40:47 +0800

    初识脚本

--- lua/a.lua
+++ lua/a.lua
@@ -2,8 +2,9 @@
 
 基础篇， 初识lua
 print 和 type都是 lua内建的函数。
-初识lua类型
+初识lua类型和运算符
 
+http://blog.csdn.net/glp3329/article/details/52511855
 --]]
 
 --[[
@@ -18,4 +19,59 @@ a = 12.3
 print(type(a)) -- 输出 number
 print(0/0) -- 输出 nan  意思就是Not a Number 不是数字
 print(type(0/0)) -- 输出 number , 意思就是nan都是number
+
+--多重赋值
+-- a = b = c = 10 不允许连等
+print(a,b,c) -- 未定义的变量默认值是nil
+a,b,c = 1,"a",2
+print(a,b,c)
+
+
+--[[
+python操作符
+
+在交互模式下，python会把运算的结果保存在 '_' 里面
+--]]
+
+-- 1.算术运算符:
+a = 10
+b = 20
+print("a+b=",a+b) --加
+print("a-b=",a-b) --减
+print("a*b=",a*b) --乘
+print("b/a=",b/a) --除
+print("b%a=",b%a) --模
+print("b^a=",b^a) --指数  lua使用这个当指数，意味着不能把它当按位异或运算了，我们有其他方法
+print("b//a=",b//a) --地板除 取整
+print("-a=",-a)
+print("*********************************")
+
+-- 2.比较操作符:
+print("a==b",a==b)
+print("a~=b",a~=b) -- 这个区别与别的 !=
+print("a>b",a>b)
+print("a<b",a<b)
+print("a>=b",a>=b)
+print("a<=b",a<=b)
+
+--a += 2 lua中没有这样的赋值运算符，需要自己写成 a = a+2
+
+-- 3.逻辑运算符:
+a = true -- 这里必须小写
+b = false
+print(a and b) 	--等价与 &&
+print(a or b) 	--等价与 ||
+print(not a) 	--等价与 !
+
+-- 4.其他运算符
+
+--字符串连接符 ..
+a = "123"
+b = "abc"
+print(a..b)
+
+--长度计算符 #
+print(#a) -- 计算字符串长度
+b = {1,2}
+print(#b) -- 计算table size
 
--- py/a.py
+++ py/a.py
@@ -2,7 +2,7 @@
 
 基础篇， 初识python
 print 和 type都是 python内建的函数。
-初识python类型
+初识python类型和运算符
 
 http://blog.csdn.net/glp3329/article/details/52511855
 '''
@@ -29,4 +29,90 @@ Traceback (most recent call last):
 ZeroDivisionError: division by zero
 '''
 #0/0
+
+#多重赋值
+a = b = c = 10
+print(a,b,c)
+a,b,c = 1,"a",2
+print(a,b,c)
+
+
+'''
+python操作符
+
+'''
+
+# 1.算术运算符:
+a = 10
+b = 20
+print("a+b=",a+b) #加
+print("a-b=",a-b) #减
+print("a*b=",a*b) #乘
+print("b/a=",b/a) #除
+print("b%a=",b%a) #模
+print("b**a=",b**a) #指数
+print("b//a=",b//a) #地板除 取整
+print("-a=",-a)
+print("*"*30)
+
+# 2.比较操作符:
+print("a==b",a==b)
+print("a!=b",a!=b)
+print("a>b",a>b)
+print("a<b",a<b)
+print("a>=b",a>=b)
+print("a<=b",a<=b)
+
+# 3.赋值运算符：
+c = 1;
+c += a
+print("c+=a = ",c)
+c -= a
+print("c-=a = ",c)
+
+#类似的还有 *= /= %= **= //=
+c *= 2
+print(c)
+c /= 2
+print(c)
+c %= 1
+print(c)
+c **= 2
+print(c)
+c //= 2
+print(c)
+
+# 4.位运算符：
+a = 3 #二进制的表示  0000 0011
+b = 10 #二进制的表示 0000 1010
+
+print("a&b =",a&b) #按位与
+print("a|b =",a|b) #按位或
+print("a^b =",a^b) #按位异或
+print("~b =",~b) #按位取反
+
+print("a<<2 =",a<<2) #按位左移
+print("b>>2 =",b>>2) #按位右移
+
+# 5.逻辑运算符:
+a = True # 这里必须大写
+b = False
+print(a and b) 	#等价与 &&
+print(a or b) 	#等价与 ||
+print(not a) 	#等价与 !
+
+
+# 6.成员运算符:
+a = "a"
+b = "abcdefg"
+print("a in b =",a in b) #判断a是否在b的里面，可以是字符串，或者是元组，序列，字典
+print("a not in b =",a not in b)
+
+# 7.标识运算符
+a = 1
+b = 2
+print("a is b =",a is b) #判断是否同一个对象
+print("a is not b =",a is not b)
+b = 1
+print("a is b =",a is b)
 
