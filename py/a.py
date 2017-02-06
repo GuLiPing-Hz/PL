ba79f59a11ce7dc4dbde54fa73ae544d5313272f
glp <1059719078@qq.com>
2017-02-06 23:24:25 +0800

    增加列表，字典

--- py/a.py
+++ py/a.py
@@ -62,6 +62,13 @@ ZeroDivisionError: division by zero
 '''
 #0/0
 
+#多重赋值
+a = b = c = 10
+print(a,b,c)
+a,b,c = 1,"a",2
+print(a,b,c)
+
+
 '''
 python操作符
 
@@ -116,6 +123,7 @@ print("a&b =",a&b) #按位与
 print("a|b =",a|b) #按位或
 print("a^b =",a^b) #按位异或
 print("~b =",~b) #按位取反
+
 print("a<<2 =",a<<2) #按位左移
 print("b>>2 =",b>>2) #按位右移
 
@@ -126,13 +134,14 @@ print(a and b) 	#等价与 &&
 print(a or b) 	#等价与 ||
 print(not a) 	#等价与 !
 
+
 # 6.成员运算符:
 a = "a"
 b = "abcdefg"
 print("a in b =",a in b) #判断a是否在b的里面，可以是字符串，或者是元组，序列，字典
 print("a not in b =",a not in b)
 
-# 7.身份运算符
+# 7.标识运算符
 a = 1
 b = 2
 print("a is b =",a is b) #判断是否同一个对象
@@ -140,70 +149,3 @@ print("a is not b =",a is not b)
 b = 1
 print("a is b =",a is b)
 
-"""
-
-Python运算符优先级
-以下表格列出了从最高到最低优先级的所有运算符：
-运算符				描述
-**					指数 (最高优先级)
-~ + -				按位翻转, 一元加号和减号 (最后两个的方法名为 +@ 和 -@)
-* / % //			乘，除，取模和取整除
-+ -					加法减法
->> <<				右移，左移运算符
-&					位 'AND'
-^ |					位运算符
-<= < > >=			比较运算符
-<> == !=			等于运算符
-= %= /= //= -= += *= **=	赋值运算符
-is is not			身份运算符
-in not in			成员运算符
-not or and			逻辑运算符
-
-"""
-
-#字符串连接符 +
-print("123"+"abc")
-
-'''
-需要说明的是 python不支持字符串和数字相加，lua可以让是数字的字符串跟数字相加
-'''
-#print("1"+2) # 报错
-
-# 字符串
-s = "Hello World"
-s1 = 'Hello World'
-s2 = '''Hello
-		World'''
-print(s,s1,s2)
-print("*"*30)
-
-# python 对字符串的访问支持正负索引
-print(s[1:]) # python 的所有索引起点都是0 区别于lua的从1开始的索引
-print(s[1:3]) # 前闭后开区间
-print(s[3:2]) # 无输出
-print(s[-5:-2]) # 前闭后开区间 ->Wor
-print("长度=",len(s)) # 查看字符串长度
-
-print("字符串复制","*"*30)
-print("字符串格式化","the first code is %s" % s)
-print("字符串成员运算符in,Hello 是否存在","Hello" in s)
-print("字符串成员运算符not in,Hello 是否不存在","Hello" not in s)
-print("原始字符串",r"Hello\n",R"World\n")
-print("字符串全部转为大写字母",s.upper())
-print("字符串全部转为小写字母",s.lower())
-print("字符串查找",s.find("ll",1)) # 返回在字符串中的开始位置 找不到返回-1
-#相比于find ，index方法找不到时会报错 还有从右边找的rfind rindex
-#print("字符串查找",s.index("lle"))
-print("字符串查找替换",s.replace("l","L",2)) # 最后一个参数是要替换的次数，不填，默认全部替换
-print("字符串查找替换2",s.replace("l","L"))
-#去除字符串左空格 lstrip ，右空格 rstrip,左右空格strip
-ss = ["a","b","c"]
-print("字符串以指定连接符连接","_".join(ss))
-print("字符串以指定字符串分隔",s.split("l"))
-print("字符串以换行符分隔",s2.splitlines(True)) # True 保留换行符 ,False 去掉换行符
-print("字符串检查指定字符串重复出现次数",s.count("l"))
-print("字符串开头检查",s.startswith("Hell"))
-print("字符串结束检查",s.endswith("world"))
-print("字符串只含数字检查",s.isdigit())
-print("字符串只含字母检查",s.isalpha())
-print("字符串只含数字或字母",s.isalnum())
