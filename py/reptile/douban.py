3ba8843e71626ceefb8fe0986dbafb7bff3162b1
guliping <1058719078@qq.com>
2017-10-31 15:14:55 +0800

    更新爬虫程序

--- py/reptile/douban.py
+++ py/reptile/douban.py
@@ -26,7 +26,16 @@ class VisitUrl(object):
 
     def visit(self,url):
         soup = None
-        if(url == None or url == "" or (url in self.had_visited)):
+        
+        if(url == None or url == ""):
+            print("空网址 ",url)
+            return soup
+
+        #去掉url中 ? 后面的垃圾内容
+        pos = url.find("?")
+        if(pos != -1):
+            url = url[:pos]
+        if(url in self.had_visited):
             print("已经访问过了!!!",url)
             return soup
 
@@ -71,13 +80,22 @@ class VisitUrl(object):
             soup = BeautifulSoup(data, 'html.parser')
         except urllib.error.HTTPError as e:#访问不了就再见
             print("http error :",url)
-            pass
+        except ConnectionRefusedError as e:#由于目标计算机积极拒绝，无法连接。
+            print("和谐1 error :",url)
+        except urllib.error.URLError as e:#由于目标计算机积极拒绝，无法连接。
+            print("和谐2 error :",url)
+        except TimeoutError as e:
+            print("超时 error :",url) as e:#由于连接方在一段时间后没有正确答复或连接的主机没有反应，连接尝试失败。
+        except http.client.RemoteDisconnected as e:#服务器关闭连接
+            print("close error :",url)
         except Exception as e:
             raise e
         
         return soup
 
     def visit_all(self,url):
+        print("="*100)
+        print("visit_all url =",url)
         soup = self.visit(url)
 
         if(soup):
@@ -85,28 +103,41 @@ class VisitUrl(object):
             print("当前页的超链接：",tag_a_lst)
             for i in range(len(tag_a_lst)):
                 item = tag_a_lst[i]
-                print("*"*100)
+                print("*"*100,i)
                 print(type(item),item)
                 value = item.get('href')
                 print("超链接地址:",url,value)
 
-                if(value == "/"):#这还是访问当前地址，忽略
+                if(type(value) == type(None)):
                     pass
-                elif(value == "javascript:;"):#百度的按钮样式
+                elif(value == "/"):#这还是访问当前地址，忽略
+                    pass
+                elif(value == "javascript:;" or value.startswith("javascript:") or value == "#"):#百度的按钮样式
                     pass
                 elif(value.startswith("http")):
                     self.visit_all(value)
                 elif(value.startswith("//")):
                     self.visit_all("http:"+value)
                 else:
-                    new_url = url
-                    xiegang_split = url.split("/")
-                    if(len(xiegang_split) > 3): # http://music.baidu.com 标准分出来应该是3个
-                        pos_end = url.find("/",8) #取出主域名
-                        if(pos_end != -1):
-                            new_url = url[:pos_end]
-                            print("新地址",new_url)
-                    self.visit_all(new_url + value)
+                    if(value.startswith("./") or value.startswith("../")):
+                        value = value[value.find("/"):]
+                    elif(not value.startswith("/")):#如果超链接地址是没带了/
+                        new_url = url[:url.rfind("/")+1]
+
+                        if(value.endswith(".html")):
+                            self.visit_all(new_url + value)
+                        else:
+                            print("文件 :",new_url+value)
+                    else:
+                        new_url = url
+                        xiegang_split = url.split("/")
+                        if(len(xiegang_split) > 3): # http://music.baidu.com 标准分出来应该是3个
+                            pos_end = url.find("/",8) #取出主域名
+                            if(pos_end != -1):
+                                new_url = url[:pos_end]
+                                print("新地址",new_url)
+                        self.visit_all(new_url + value)
+                    
 
 def get_douban():
     """
@@ -118,6 +149,10 @@ def get_douban():
     visiter.visit_all("https://www.baidu.com")
 
 if __name__ == '__main__':
+
+    import sys
+    sys.setrecursionlimit(10**9)  # set the maximum depth as 10的10次方
+
     print("开始访问网站...")
     get_douban()
     print("访问到此结束")
