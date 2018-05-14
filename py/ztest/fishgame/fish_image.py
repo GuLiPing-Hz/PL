8e7c75a50f1a99177e33b7715d124fd77705eba4
guliping <1058719078@qq.com>
2018-05-14 13:29:47 +0800

    11

--- py/ztest/fishgame/fish_image.py
+++ py/ztest/fishgame/fish_image.py
@@ -24,6 +24,8 @@ def encFile(path,file):
 
     if file[-4:] != ".png" and file[-5:] != ".webp" and file[-4:] != ".jsc":
         return
+    if(file == "loading.js"):
+        return
 
     fullpath=file_helper.join(path,file)
     print("cur file = ",fullpath)
@@ -51,7 +53,7 @@ def encFile(path,file):
         fp.write(newData)
         fp.flush()
 
-def temp(path):
+def temp(path,recursive=False):
     #图片处理的一些列操作 后缀.webp改成.png
 
     #遍历目录，取出智图软件处理后的webp图片，放到原来的位置，并重命名为png
@@ -61,7 +63,7 @@ def temp(path):
     #加密图片
     print("开始加密图片文件...")
     #临时文件加密
-    file_helper.Diskwalk(path,False).walk(encFile);
+    file_helper.Diskwalk(path,recursive).walk(encFile);
     print("加密图片文件完成")
 
 def main(path):
@@ -89,13 +91,15 @@ if __name__ == '__main__':
     #以后路径统一使用 '/ 请勿使用 '\\'
     
     #运行一次是加密，运行第二次是解密
-    IMAGE_KEY = "aaazhejiangfanyu2018"
+    IMAGE_KEY = "aaazhejiangfanyu2018" #主包加密
+    # IMAGE_KEY = "spzhejiangfanyu2018" #sp渠道加密
 
     # main("D:/glp/GitHub/fishjs")
-    # temp("D:/glp/work/UI/temp")
+    # temp("D:/glp/work/UI/temp",False);#是否遍历子目录
 
     #编译工程 最后一步，加密jsc，跟苹果斗智斗勇
     enc_jss("D:/glp/Github/fishjs/third_part/jsc/src")
+    # enc_jss("D:/glp/Github/fishjs/third_part/jsc_sp")
 
     #热更新 jsc加密一下
     # enc_jss("D:/glp/work/temp/update/src")
