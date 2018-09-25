99cc370ef10688cd36727aa49485dc54a23c6ba0
guliping <1058719078@qq.com>
2018-09-25 17:26:58 +0800

    开始重新学习javascript，从基础学起

--- js/test1.js
+++ js/test1.js
@@ -1,6 +1,34 @@
+/**
+ * 
+ * 开始新的JavaScript学习
+ */
 
+/* 变量提升
+ *  JavaScript 引擎的工作方式是，先解析代码，获取所有被声明的变量，然后再一行一行地运行。
+ *  这造成的结果，就是所有的变量的声明语句，都会被提升到代码的头部，这就叫做变量提升（hoisting）。
+ */
+console.log(a);//这里变量提升
 var a = 1;
-var b = 2;
+console.log(a);
 
-console.log('Manifest successfully generated'+(a+b));
+/**
+ * 变量提升
+ * 上面的代码等价于:
+ * var a;
+ * console.log(a);
+ * a = 1;
+ * console.log(a);
+ * 
+ */
+
+ /**
+  * 标识符
+  * 
+  * 第一个字符，可以是任意 Unicode 字母（包括英文字母和其他语言的字母），以及美元符号（$）和下划线（_）。
+  * 第二个字符及后面的字符，除了 Unicode 字母、美元符号和下划线，还可以用数字0-9
+  */
+
+var 临时变量 = 1;
+console.log("临时变量="+临时变量);
+
 
