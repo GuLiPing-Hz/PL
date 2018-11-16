3056a5824868baade64c1b336fc8c6af5d7896be
guliping <1058719078@qq.com>
2018-11-16 19:32:23 +0800

    11

--- lua/fish/testFish.lua
+++ lua/fish/testFish.lua
@@ -55,4 +55,68 @@ function CompileVersion(verA,verB)
     end
 end
 
-print(CompileVersion("1.0.0.0","1.0.0"))
+-- print(CompileVersion("1.0.0.0","1.0.0"))
+function getOrder()
+    local order = { 1, 2, 3 }
+    local AskRate = {10,30,60}
+    local askOrder = {}
+    for i = 1, 3 do
+        sum = 0
+        for i in ipairs(order) do
+            sum = sum + AskRate[order[i]]
+        end
+        local rate = math.random(sum)
+        -- print(rate)
+
+        local start = 0
+        local finish = 0
+        for i in ipairs(order) do
+            finish = start + AskRate[order[i]]
+            if rate <= finish and rate > start then
+                table.insert(askOrder, order[i]);
+                table.remove(order, i)
+                break
+            end
+            start = finish
+        end
+    end
+
+    local str = ""
+    for i,v in ipairs(askOrder) do str = str..","..v end
+    print(str)
+end
+
+local __fish_rate_list = {
+{1,2,3,4,5,6},
+{1,2,3,4,5,6},
+{1,2,3,4,5,6},
+{1,2,3,4,5,6},
+{1,2,3,4,5,6},
+{1,2,3,4,5,6}}
+function refresChair()
+    for m = 1, #__fish_rate_list do
+        local i, j, temp
+        local l = #__fish_rate_list[m]
+        for i = 1, l do
+            j = math.random(l)
+            temp = __fish_rate_list[m][i]
+            __fish_rate_list[m][i] = __fish_rate_list[m][j]
+            __fish_rate_list[m][j] = temp
+        end
+    end
+
+    local str = ""
+    for i,v in ipairs(__fish_rate_list) do 
+        for j,v2 in ipairs(v) do
+            str = str..","..v2
+        end
+        str = str .. "\n"
+    end
+    print(str)
+end
+
+math.randomseed(os.time())
+for i=1,10 do
+    refresChair()
+    print(string.rep("*",100))
+end
