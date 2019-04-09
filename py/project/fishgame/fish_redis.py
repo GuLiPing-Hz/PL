1afad4244af3db2bb610e51b8457d69e5eee2384
guliping <1058719078@qq.com>
2019-04-09 11:36:05 +0800

    redis增加正则表达式检索

--- py/project/fishgame/fish_redis.py
+++ py/project/fishgame/fish_redis.py
@@ -1,5 +1,6 @@
 import redis
 import pymysql
+import re
 # import redaline
 
 def resetUsrCard(uid):
@@ -8,7 +9,7 @@ def resetUsrCard(uid):
 	
 def cleanUsers(pwd,pwd2,start=None,end=None):
 	if not start or not end:
-		connection = pymysql.connect(host='121.196.203.52',
+		connection = pymysql.connect(host='127.0.0.1',
 	                             user="root",
 	                             password=pwd,
 	                             db="Buyu",
@@ -29,14 +30,14 @@ def cleanUsers(pwd,pwd2,start=None,end=None):
 			vMax = cursor.fetchone()
 			end = vMax["uid"]
 
-	r = redis.Redis(host='121.196.203.52', port=6379, db=0,password=pwd2)
+	r = redis.Redis(host='127.0.0.1', port=6379, db=0,password=pwd2)
 	# print(dir(r))
 	for i in range(start,end):
 		print("del usr_"+str(i),r.delete("usr_"+str(i)))
 		r.delete("usrStat_"+str(i))
 
 def cleanUsrByUids(uids,pwd,host=None):
-	host = host or '121.196.203.52'
+	host = host or '127.0.0.1'
 	r = redis.Redis(host=host, port=6379, db=0,password=pwd)
 	# print(dir(r))
 
@@ -44,20 +45,19 @@ def cleanUsrByUids(uids,pwd,host=None):
 		uid = uids[i]
 		print("del usr_"+str(uid),r.delete("usr_"+str(uid)))
 
-def cleanUsrAndStat(pwd,pattern="",host=None):
-	host = host or '121.196.203.52'
+def cleanUsrAndStat(pwd,pattern=None,host=None,):
+	host = host or '127.0.0.1'
 	r = redis.Redis(host=host, port=6379, db=0,password=pwd)
 	# print(dir(r))
 
 	keys = r.keys()
 	for i in range(len(keys)):
 		theKey = (keys[i]).decode("utf-8") #byte -> str
-		print(theKey)
-		if pattern == "":
+		print("key=",theKey)
+		if not pattern:
 			print("del "+theKey,r.delete(theKey))
-		elif theKey.startswith(pattern):
+		elif pattern.match(theKey):
 			print("del "+theKey,r.delete(theKey))
-
 
 def testRedis():
 	r = redis.Redis(host='121.196.203.52', port=6379, db=0,password=redisPwd)
@@ -82,17 +82,42 @@ if __name__ == '__main__':
 		redisPwd = f.readline()
 	print(mySqlPwd1,mySqlPwd2,redisPwd)
 
+	# testRedis()
 
 	# cleanUsers(mySqlPwd1,redisPwd) #清除所有人的redis缓存，慎用 -这个方法最low，从数据获取到uid最大的
 	# cleanUsrByUids([100090],redisPwd,"127.0.0.1") #清除指定uid
 
-	# cleanUsrByUids([167362],redisPwd) #清除指定uid
-
 	# cleanUsrAndStat(redisPwd,"wx_") #清除微信登录授权信息
 	# cleanUsrAndStat(redisPwd,"usr_") #清除用户缓存信息
 	# cleanUsrAndStat(redisPwd,"usr_today_") #清除用户当日缓存信息
-	cleanUsrAndStat(redisPwd)#清除所有人的redis缓存，慎用
+
+	# cleanUsrAndStat(redisPwd)#清除所有的redis缓存，慎用
+
+	#*匹配0次多次 +匹配一次多次 ?匹配0次1次(非贪婪)
+	#\d 匹配所有数字 \D匹配任意非数字 ^匹配开头 $匹配末尾 \w匹配字母数字下划线 \W	匹配非字母数字及下划线
+	pattern = None #这样会清理redis中所有的缓存信息
+	#下面两个很少会用
+	#### pattern = re.compile("^token_\\d+")#只匹配Token信息 ->建议还是用r比较方便，不然要写很多的\转义符
+	#### pattern = re.compile(r"^WX_\d+")#只匹配WX认证信息 ->建议还是用r比较方便，不然要写很多的\转义符
+
+	# pattern = re.compile(r"^usr_checkin_\d+")#只匹配用户签到信息
+	# pattern = re.compile(r"^usr_checkin7_\d+")#只匹配用户7日签到信息
+	# pattern = re.compile(r"^usr_\d+")#只匹配用户信息
+	# pattern = re.compile(r"^usr_today_\d+")#只匹配用户今日信息
+	# pattern = re.compile(r"^usr_today_m_\d+")#只匹配用户今日更多信息
+
+	# #\s 匹配所有空白字符=[\t\n\r\f]， \S 匹配所有非空白字符
+	# pattern = re.compile(r"^usr_today_\S+")#匹配用户的所有今日信息，
+	# pattern = re.compile(r"^usr_\S+")#匹配用户的所有信息，，在线列表的redis名字后面要换一下。。
+	
+	print("pattern=",pattern)
+	# if pattern:
+	# 	print("match=",pattern.match("usr_checkin7_165272"))
+
+	host = "121.196.203.52"
+	# host = "192.168.100.2"
+	cleanUsrAndStat(redisPwd,pattern,host)#清除所有的redis缓存，慎用
 
 	
-	# testRedis()
+	
 
