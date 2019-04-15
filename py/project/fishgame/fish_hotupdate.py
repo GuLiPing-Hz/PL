5d6584c1cf080db0f5bc2317478279d0e6283c07
guliping <1058719078@qq.com>
2019-04-15 23:23:41 +0800

    11

--- py/project/fishgame/fish_hotupdate.py
+++ py/project/fishgame/fish_hotupdate.py
@@ -1,12 +1,11 @@
 import json
-import file_helper
 import os
 # 当我们脚本是主入口的时候,如果要引入上层目录的脚本,那么只能通过添加sys.path的方式
 # 然后并不推荐这样写,这样是由于设计目录的时候原本就不规范导致
 # 正确的方法应该是把我们自己写的脚本都放到一个目录，并且子目录的脚本不能引用上级目录的模块
 import sys
 sys.path.append(__file__[:__file__.rfind("\\")]+"\\..")
-
+import file_helper
 """
 本文件是依赖python3
 
